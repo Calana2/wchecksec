3a9219abcaf16065dde09588ce6b699cbf87971e
Calana2 <kalcast00@proton.me>
2025-08-22 14:21:38 -0400

    fixed script

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "Installing..."
+if [ ! -d "./bin" ]; then
+  mkdir ./bin
+fi
 go build -o ./bin/wchecksec wchecksec.go
 cp -R ./bin/* /usr/local/bin
+rm ./bin/wchecksec
 echo "Done."
 
